6c246665f0c921d7a38a6249a30c7e36ca34c75f
Lee.WinMore <zzlxxi@163.com>
2019-08-20 23:05:09 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 image=verify-code:1.0.0-${GIT_COMMIT:0:7}
 mirro=registry.cn-shanghai.aliyuncs.com/ink-icopy
-docker build -t ${image} .
-docker tag ${image} ${mirro}/${image} &&
-docker push ${mirro}/${image}
+docker build -t ${image} . && docker tag ${image} ${mirro}/${image} && docker push ${mirro}/${image}
