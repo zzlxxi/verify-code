219ea51855f6ef0dd9e21767327c1aea9e91b156
zzlxxi <zzlxxi@163.com>
2019-08-30 18:40:15 +0800

    add mybatis plus

--- src/main/java/ink/icopy/verifycode/service/UserService.java
+++ src/main/java/ink/icopy/verifycode/service/UserService.java
@@ -3,11 +3,14 @@ package ink.icopy.verifycode.service;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import ink.icopy.verifycode.entity.User;
 import ink.icopy.verifycode.mapper.UserMapper;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /** @author lizhengguang */
+@Slf4j
 @Service
 public class UserService {
   private UserMapper userMapper;
@@ -19,7 +22,7 @@ public class UserService {
   public List<User> queryUserList() {
     QueryWrapper<User> query = new QueryWrapper<User>().eq("id", 3).or(q -> q.eq("name", "Sandy"));
     List<User> users = userMapper.selectList(query);
-    users.forEach(u -> System.out.println(u));
+    users.forEach(u -> log.info(u.toString()));
     return users;
   }
 }
