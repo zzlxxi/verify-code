b1f45fc9c785d2c96c59a5acab611bc2f3eb2fc0
zzlxxi <zzlxxi@163.com>
2019-09-05 13:35:17 +0800

    add thymeleaf1

---
+++ src/test/java/ink/icopy/verifycode/template/ThymeleafTemplateTest.java
@@ -0,0 +1,14 @@
+package ink.icopy.verifycode.template;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+
+@SpringBootTest
+public class ThymeleafTemplateTest {
+
+  @Test
+  public void html() {
+    TemplateEngine templateEngine = new TemplateEngine();
+  }
+}
